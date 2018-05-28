cc475e1274dc8674acc3dd47955f7a293d44bf9a
sam-pop <poplovitch.samuel@gmail.com>
2018-05-27 21:45:04 -0400

    geoJSON added

---
+++ assets/javascript/POI.js
@@ -0,0 +1,228 @@
+var NMwest = {
+  "type": "FeatureCollection",
+  "features": [{
+      "type": "Feature",
+      "properties": {
+        "name": "Washington Monument"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-77.03524231910706,
+          38.889479579773514
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "WWII Memorial"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-77.04032778739929,
+          38.88942947447528
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Best View Point"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-77.0367282629013,
+          38.88947122889294
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Vietnam Veterans Memorial"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-77.048042, 38.890979]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "The Three Soldiers"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-77.04817593097687,
+          38.89053595825016
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Vietnam Women's Memorial"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-77.0469743013382,
+          38.89049420445836
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Lincoln Memorial"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-77.0501446723938,
+          38.889262456559266
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Arts of Peace Statues"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-77.05215632915497,
+          38.889792737086694
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Art of War Statue"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-77.05195784568787,
+          38.888469116095486
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "MLK Jr Memorial"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-77.044453, 38.885976]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "FDR Memorial"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-77.04321920871735,
+          38.88322867101698
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Jefferson Memorial"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-77.03657269477844,
+          38.88140381972525
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Reflecting Pool"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-77.04468905925751,
+          38.889345965566385
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "DC War Memorial"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-77.04351425170897,
+          38.887617309102474
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Korean War Veterans Memorial"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-77.04754292964934,
+          38.8877968575045
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "John Ericsson Memorial"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-77.05019094049929,
+          38.8866866774098
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "56 Signers of the Declaration of Independence Memorial"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-77.04287052154541,
+          38.891107982726105
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Jefferson Pier Stone"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-77.03654050827026,
+          38.889801087929484
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "John Paul Jones Memorial"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [-77.03946948051453,
+          38.888226936715064
+        ]
+      }
+    }
+  ]
+}
--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,40 +1,7 @@
 // Global Variables
 var baseCoords = [38.889488, -77.035285];
 var mymap = L.map('mapid').setView(baseCoords, 16);
-
-var currentLocIcon = L.icon({
-    iconUrl: './assets/images/map-marker-person.png',
-    iconSize: [38, 42],
-    iconAnchor: [20, 36],
-    popupAnchor: [-3, -76],
-    shadowUrl: '',
-    shadowSize: [68, 95],
-    shadowAnchor: [22, 94]
-});
-var poiIcon = L.icon({
-    iconUrl: './assets/images/map-marker.png',
-    iconSize: [38, 42],
-    iconAnchor: [20, 44],
-    popupAnchor: [-3, -76],
-    shadowUrl: '',
-    shadowSize: [68, 95],
-    shadowAnchor: [22, 94]
-});
-//mapbox.streets-satellite <- maybe use
-L.tileLayer('https://api.tiles.mapbox.com/v4/mapbox.outdoors/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoic2FtLXBvcCIsImEiOiJjamhucjhhNXgwNTE0MzZwYWQxenprNG5kIn0.9c-GiLb45NYrZeAiy3TZ6w', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    minZoom: 14,
-    id: 'mapbox.outdoors',
-    accessToken: 'pk.eyJ1Ijoic2FtLXBvcCIsImEiOiJjamhucjhhNXgwNTE0MzZwYWQxenprNG5kIn0.9c-GiLb45NYrZeAiy3TZ6w'
-}).addTo(mymap);
-
-
-
-var marker = L.marker(baseCoords, {
-    icon: poiIcon
-});
-var polygon = L.polygon([
+var nationalMallPoly = L.polygon([
     [38.892239, -77.052317],
     [38.888548, -77.052666],
     [38.885876, -77.049817],
@@ -45,53 +12,69 @@ var polygon = L.polygon([
     [38.892089, -77.014761]
 ]);
 
-// add to map
-marker.addTo(mymap);
-polygon.addTo(mymap);
+var currentLocIcon = L.icon({
+    iconUrl: './assets/images/map-marker-person.png',
+    iconSize: [38, 42],
+    iconAnchor: [20, 36],
+    popupAnchor: [-3, -76],
+    shadowUrl: '',
+    shadowSize: [68, 95],
+    shadowAnchor: [22, 94]
+});
 
-// binding objects to popups
-marker.bindPopup("Start here").openPopup();
-// polygon.bindPopup("<b>The National Mall<b>");
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/mapbox.outdoors/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoic2FtLXBvcCIsImEiOiJjamhucjhhNXgwNTE0MzZwYWQxenprNG5kIn0.9c-GiLb45NYrZeAiy3TZ6w', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    minZoom: 14,
+    id: 'mapbox.outdoors',
+    accessToken: 'pk.eyJ1Ijoic2FtLXBvcCIsImEiOiJjamhucjhhNXgwNTE0MzZwYWQxenprNG5kIn0.9c-GiLb45NYrZeAiy3TZ6w'
+}).addTo(mymap);
+
+
+// add to map the NM polygon
+// nationalMallPoly.addTo(mymap); //FIXME: save for later
 
 var popup = L.popup();
 
 function onMapClick(e) {
     popup.setLatLng(e.latlng).setContent("You clicked the map at " + e.latlng.toString()).openOn(mymap);
 }
-
 mymap.on('click', onMapClick);
 
-// show my current location
-var myLoc = mymap.locate({
-    setView: true,
-    maxZoom: 16,
-    watch: true,
-    enableHighAccuracy: true
-});
+// show my current location //FIXME: save for later
+// var myLoc = mymap.locate({
+//     setView: true,
+//     maxZoom: 16,
+//     // watch: true,
+//     enableHighAccuracy: true
+// });
 
-// functions that can be called when my location is found and on error
+// current location success / error functions
 function onLocationFound(e) {
-    // mymap.removeLayer(currentMarker);
+    // mymap.removeLayer(currentMarker); //FIXME: save for later
 
     var radius = e.accuracy / 2;
-
     var currentMarker = L.marker(e.latlng, {
         icon: currentLocIcon
     });
-
     currentMarker.addTo(mymap)
         .bindPopup("You are within " + radius + " meters from this point").openPopup();
-
     L.circle(e.latlng, radius).addTo(mymap);
 }
 
 function onLocationError(e) {
     alert(e.message);
 }
+
 mymap.on('locationfound', onLocationFound);
 mymap.on('locationerror', onLocationError);
 
-
-// L.marker(baseCoords, {
-//     icon: myIcon
-// }).addTo(mymap);
+$(document).ready(function () {
+    // displays the West markers
+    var NMwestMarkers = L.geoJSON(NMwest, {
+        onEachFeature: function (feature, layer) {
+            layer.bindPopup(feature.properties.name);
+        }
+    }).addTo(mymap);
+});
